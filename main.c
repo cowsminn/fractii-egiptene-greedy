4bcc5afacbac52cef3b3f37a6d42e99147d59dc6
cowsmin <cosmin.mnj@yahoo.com>
2023-11-08 20:18:48 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,82 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-//dividee et impera prob 5, Caravan Cosmin 164
 
-int cmp(const void *a, const void *b)
+void egipt(int a, int b)
 {
-
-  return (*(int*)a - *(int*)b);
-
-}
-
-void swap(int *x, int *y)
-{
-    int aux = *x;
-    *x = *y;
-    *y = aux;
-}
-
-int m_assert(int *v, int n, int k)
-{
-    qsort(v,n,sizeof(int),cmp);
-    return v[k];
-}
-
-int divide(int *v, int stg, int dr)
-{
-    int pivot = v[dr];
-    int i = stg - 1;
-    for (int j = stg; j < dr; j++)
-    {
-        if (v[j] < pivot)
-        {
-            i++;
-            swap(&v[i], &v[j]);
-        }
+    assert(b != 0 && "nedifinit - numitorul == 0"); /// nu am stiut exact cum as putea sa folosesc
+                                                    /// assert pt outputul de la void() recursiv
+    if(a == 0){
+        printf("0");
+        return;
     }
-    swap(&v[i+1], &v[dr]);
-    return i+1;
+    if (b % a == 0) {
+        printf("1/%d", b / a);
+        return;
+    }
+    if (a % b == 0) {
+        printf("%d", a / b);
+        return;
+    }
+    if (a > b) {
+        printf("%d + ", a / b);
+        egipt(a % b, b);
+        return;
+    }
+    int x = b / a + 1;
+    printf("1/%d + ", x);
+    egipt(a * x - b, b * x);
 }
-
-int min_k(int *v, int stg, int dr, int k)
-{
-    if (stg == dr)
-        return v[stg];
-    int pivot = divide(v, stg, dr);
-    if (k == pivot)
-        return v[pivot];
-    else if (k < pivot)
-        return min_k(v, stg, pivot-1, k);
-    else
-        return min_k(v, pivot+1, dr, k);
-
-}
-
 int main()
 {
-    FILE *f = fopen("date.in","r");
-    FILE *g = fopen("date.out","w");
-    if(f == NULL)
-    {
-        printf("eroare fisier");
-        return 1;
-    }
+    int a, b; /// a - numaratorul b - numitorul
 
-    int n,k;
-    fscanf(f,"%d%d",&n,&k);
+    printf("Numaratorul: ");
+    scanf("%d", &a);
+    printf("Numitorul: ");
+    scanf("%d", &b);
+    printf("Fractia %d/%d se scrie in fractii egiptene astfel:\n", a, b);
 
-    int *v = (int*)malloc(sizeof(int) * n);
-    for(int i = 0; i < n; i++)
-        fscanf(f,"%d",&v[i]);
-
-    assert(k > 0 && k <= n);
-    assert(min_k(v,0,n-1,k-1) == m_assert(v,n,k-1));
-
-    fprintf(g,"%d ",min_k(v,0,n-1,k-1)); // luam k-1 pentru ca incepe cnt de 0
-    free(v);
+    egipt(a, b);
 
     return 0;
-
-
 }
